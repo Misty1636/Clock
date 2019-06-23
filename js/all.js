93b05395f81bd6a929ea54dc451ab063ab3f037c
Misty1636 <a3722041a@yahoo.com.tw>
2019-06-24 02:27:40 +0800

    update js

--- js/all.js
+++ js/all.js
@@ -9,7 +9,7 @@
 
     const sec = s / 60 * 360;
     const min = m / 60 * 360 + s / 10; // 秒針動，分針要跟著慢慢移動
-    const hour = h / 24 * 360 + m / 10; // 分針動，時針要跟著慢慢移動
+    const hour = h / 12 * 360 + m / 10; // 分針動，時針要跟著慢慢移動
     if (s === 0) { // 為了解決 transition 倒轉，另一解法直接累加deg 不歸0
       secss.removeAttribute('style', 'transition: all .3s cubic-bezier(0.82,-0.32, 0.29, 1.51)');
     } else {
